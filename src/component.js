5daac01b67420351ef8d5447cb00bb66c4db7883
Anacelia Gillette <acgillette426@gmail.com>
2017-07-10 11:45:03 -0700

    Generates a random landscape each time

--- src/component.js
+++ src/component.js
@@ -1,112 +1,16 @@
 var SimplexNoise = require('simplex-noise');
-//
-// AFRAME.registerComponent('terrain', {
-//   schema: {
-//     width: {type : 'number', default: 1000},
-//     height: {type: 'number', default: 1000},
-//     flying: {type: 'number', default: 0}
-//   },
-//
-//   update: function(oldData) {
-//     var data = this.data;
-//     var el = this.el;
-//     var scl = 20;
-//     var simplex = new SimplexNoise();
-//
-//
-//
-//
-//     this.rows = data.height  / scl;
-//     this.cols = data.width  / scl;
-//     if(this.terrainArray === undefined) {
-//       this.terrainArray = new Array();
-//       for(var y = 0; y < this.rows; y++) {
-//         this.terrainArray.push(new Array(this.cols));
-//         for(var x = 0; x < this.cols; x++) {
-//           this.terrainArray[y][x] = 0;
-//         }
-//       }
-//     }
-//
-//     var xoff = 0;
-//     var yoff = data.flying;
-//     data.flying -= 0.1;
-//
-//     for(var y = 0; y < this.rows; y++) {
-//       this.terrainArray.push(new Array(this.cols));
-//       for(var x = 0; x < this.cols; x++) {
-//         this.terrainArray[y][x] = simplex.noise2D(xoff, yoff) * 20;
-//         xoff += 0.2;
-//       }
-//       yoff += 0.2;
-//     }
-//
-//
-//
-//
-//     var geometry = new THREE.Geometry();
-//     var verticesCount = 0;
-//
-//     for(var y = 0; y < this.rows - 3; y++) {
-//       for(var x = 0; x < this.cols; x++) {
-//         geometry.vertices.push(
-// 	          new THREE.Vector3( x * scl, y * scl, this.terrainArray[y + 1][x] ),
-// 	          new THREE.Vector3( x * scl, (y + 1) * scl, this.terrainArray[y + 2][x] ),
-//             new THREE.Vector3( (x + 1) * scl, y * scl, this.terrainArray[y + 3][x] )
-//         );
-//         geometry.faces.push( new THREE.Face3( verticesCount, (verticesCount + 1), (verticesCount + 2) ));
-//         verticesCount += 3;
-//
-//       }
-//
-//
-//     }
-//
-//     var material = new THREE.MeshBasicMaterial( { wireframe: true } );
-//
-//     var mesh = new THREE.Mesh( geometry, material );
-//     mesh.drawMode = THREE.TriangleStripDrawMode;
-//
-//     el.setObject3D('mesh', mesh);
-//
-//
-//   },
-//   tick: function(time, timeDelta) {
-//     // this.update();
-//     var el = this.el;
-//     var terrain = el.getObject3D('mesh').geometry;
-//     var data = this.data;
-//     var simplex = new SimplexNoise();
-//
-//
-//     terrain.verticesNeedUpdate = true;
-//
-//     var xoff = 0;
-//     var yoff = data.flying;
-//     data.flying -= 0.1;
-//
-//     for(var y = 0; y < this.rows; y++) {
-//       this.terrainArray.push(new Array(this.cols));
-//       for(var x = 0; x < this.cols; x++) {
-//         this.terrainArray[y][x] = simplex.noise2D(xoff, yoff) * 10;
-//         xoff += 0.2;
-//       }
-//       yoff += 0.2;
-//     }
-//   }
-//
-//
-//
-// });
+function map(value, start1, stop1, start2, stop2) {
+    return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
+}
 
 AFRAME.registerComponent('terrain', {
     schema: {
-      flying: {type: 'number', default: 0}
+      flying: {type: 'number', default: 0},
+      heightMap: {type: 'array', default: []}
   },
   init: function() {
     var loader = new THREE.OBJLoader();
     var that = this;
-  // load a resource
     loader.load(
       './src/terrain.obj',
       function ( object ) {
@@ -115,52 +19,39 @@ AFRAME.registerComponent('terrain', {
           if ( child instanceof THREE.Mesh ) {
             child.material = material;
             child.geometry = new THREE.Geometry().fromBufferGeometry( child.geometry );
+            var simplex = new SimplexNoise();
+            for(var i = 0; i < child.geometry.vertices.length; i++) {
+              var v = child.geometry.vertices[i];
+              that.data.heightMap.push(map(simplex.noise2D(v.x / 1024.0, v.y / 1024.0), 0, 1, -100, 100));
+            }
           }
         } );
         that.el.setObject3D('mesh', object);
       }
     );
+
+  },
+  update: function(oldData) {
+
   },
   tick: function(time, timeDelta) {
-        // var el = this.el;
-        // if(el.getObject3D('mesh')) {
-        //   var terrain = el.getObject3D('mesh').children[0].geometry;
-        //   // console.log(terrain.vertices);
-        //
-        //   // var terrain = el.getObject3D('mesh').geometry;
-        //   var data = this.data;
-        //   var simplex = new SimplexNoise();
-        //   //
-        //   //
-        //   terrain.verticesNeedUpdate = true;
-        //   //
-        //   // var xoff = 0;
-        //   // var yoff = data.flying;
-        //   data.flying -= 0.1;
-        //   var yoff = data.flying;
-        //   var xoff = 0;
-        //
-        //   //
-        //   for (var i = 0; i < terrain.vertices.length; i++)
-        //     {
-        //       var v = terrain.vertices[i];
-        //       v.z = simplex.noise2D(xoff, yoff) * 10;
-        //       // do stuff with v...
-        //       yoff += 0.2;
-        //       xoff += 0.2;
-        //
-        //     }
-        //
-        //   // for(var y = 0; y < this.rows; y++) {
-        //   //   this.terrainArray.push(new Array(this.cols));
-        //   //   for(var x = 0; x < this.cols; x++) {
-        //   //     this.terrainArray[y][x] = simplex.noise2D(xoff, yoff) * 10;
-        //   //     xoff += 0.2;
-        //   //   }
-        //   //   yoff += 0.2;
-        //   // }
 
+        var el = this.el;
+        if(el.getObject3D('mesh')) {
+          var terrain = el.getObject3D('mesh').children[0].geometry;
+          var data = this.data;
+          var simplex = new SimplexNoise();
+
+          terrain.verticesNeedUpdate = true;
+
+          for (var i = 0; i < data.heightMap.length; i++)
+            {
+              var v = terrain.vertices[i];
+              v.z = data.heightMap[i];
+
+            }
         }
+      }
 
 
 });
