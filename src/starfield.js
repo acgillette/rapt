ace60e62fe05299d9959666e29d1bf19405f3fa1
Anacelia Gillette <acgillette426@gmail.com>
2017-07-10 13:42:35 -0700

    Basic starfield generation done

--- src/starfield.js
+++ src/starfield.js
@@ -5,13 +5,17 @@ var stars = [];
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
-for(var i = 0; i < 1000; i++) {
+for(var i = 0; i < 2000; i++) {
   stars.push([ getRandomInt(-1000, 1000), getRandomInt(-1000, 1000), getRandomInt(-1000, 1000) ]);
 }
 
 console.log(stars);
 console.log(points);
-points.components.point.setPoints(stars);
+console.dir(points.components);
+setTimeout(function() {
+    points.components.point.setPoints(stars);
+    console.log(points.components.point);
+}, 1000);
