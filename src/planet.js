144713b8047428bd74f1db7180d93860becddd57
Anacelia Gillette <acgillette426@gmail.com>
2017-07-12 12:05:48 -0700

    Planet size has been randomized

--- src/planet.js
+++ src/planet.js
@@ -1,6 +1,12 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 AFRAME.registerComponent('planet', {
   init: function() {
-    var geometry = new THREE.SphereGeometry(50, 16, 16);
+    var geometry = new THREE.SphereGeometry(getRandomInt(50, 200), 16, 16);
     var material = new THREE.MeshBasicMaterial( { wireframe: true } );
     var sphere = new THREE.Mesh(geometry, material);
 
