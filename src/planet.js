d633017cd81f666630c2594bf8019a7c9861b61e
Anacelia Gillette <acgillette426@gmail.com>
2017-07-13 12:26:01 -0700

    Wrote a simple shader for the planets

--- src/planet.js
+++ src/planet.js
@@ -1,3 +1,5 @@
+var $ = require('jquery');
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -5,11 +7,22 @@ function getRandomInt(min, max) {
 }
 
 AFRAME.registerComponent('planet', {
+  schema: {
+    moons: {type: 'int', default: 0},
+    rings: {type: 'boolean', default: 'false'}
+
+  },
   init: function() {
+    var fShader = $('#fragmentshader');
     var texture = new THREE.TextureLoader().load( "./planetTexture.png" );
 
-    var geometry = new THREE.SphereGeometry(getRandomInt(50, 250), 20, 20);
-    var material = new THREE.MeshPhongMaterial({ transparent: false, map: THREE.ImageUtils.loadTexture('./src/planetTexture.png') });
+    var geometry = new THREE.SphereGeometry(getRandomInt(100, 250), 20, 20);
+    // var material = new THREE.MeshBasicMaterial( { wireframe: true } );
+    // var material = new THREE.MeshPhongMaterial({ transparent: false, map: THREE.ImageUtils.loadTexture('./src/planetTexture.png') });
+
+    var material = new THREE.ShaderMaterial({
+      fragmentShader: fShader.text()
+    });
     var sphere = new THREE.Mesh(geometry, material);
 
     this.el.setObject3D('mesh', sphere);
