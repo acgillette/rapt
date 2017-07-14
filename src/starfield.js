5f409ac38c0557e78850de1fb103b7786e9683fa
Anacelia Gillette <acgillette426@gmail.com>
2017-07-13 22:16:17 -0700

    Um kind of made it better I guess

--- src/starfield.js
+++ src/starfield.js
@@ -5,11 +5,11 @@ var stars = [];
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+  return (Math.floor(Math.random() * (max - min)) + min);
 }
 
-for(var i = 0; i < 10000; i++) {
-  stars.push([ getRandomInt(-1000, 1000), getRandomInt(-1000, 1000), getRandomInt(-1000, 1000) ]);
+for(var i = 0; i < 30000; i++) {
+  stars.push([ getRandomInt(-5000, 5000), getRandomInt(-5000, 5000), getRandomInt(-5000, 5000) ]);
 }
 
 setTimeout(function() {
