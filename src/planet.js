9eb7cbf9a5b8658be9f61d3efb9941bc9d4d7e09
Anacelia Gillette <acgillette426@gmail.com>
2017-07-14 11:17:16 -0700

    Rings are rotated onto the planet correctly

--- src/planet.js
+++ src/planet.js
@@ -53,12 +53,19 @@ AFRAME.registerComponent('planet', {
 
     var sphere = new THREE.Mesh(geometry, material);
 
-    var ringGeometry = new THREE.RingGeometry(radius, radius + getRandomInt(50, 70), 25);
-    var ringMaterial = new THREE.MeshBasicMaterial( {wireframe: true});
-    var ring = new THREE.Mesh(ringGeometry, ringMaterial);
 
     this.el.setObject3D('mesh', sphere);
-    this.el.setObject3D('ring', ring);
+
+    console.log(this.data.rings);
+    if (this.data.rings === true) {
+      var ringGeometry = new THREE.RingGeometry(radius + 10, radius + getRandomInt(50, 70), 25);
+      var ringMaterial = new THREE.MeshBasicMaterial( {wireframe: true});
+      var ring = new THREE.Mesh(ringGeometry, ringMaterial);
+      ring.rotation.x = Math.PI / 2;
+
+      this.el.setObject3D('ring', ring);
+
+    }
 
 
   }
